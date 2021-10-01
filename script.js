8414e40d52f673d3f626fa9a59b2a1267c664fe1
christa-baccas <christa.baccas@gmail.com>
2021-10-01 12:10:53 -0500

    added fetch for API

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
-// GIVEN a weather dashboard with form inputs
-// WHEN I search for a city
-// THEN I am presented with current and future conditions for that city and that city is added to the search history
-// WHEN I view current weather conditions for that city
-// THEN I am presented with the city name, the date, an icon representation of weather conditions, the temperature, the humidity, the wind speed, and the UV index
-// WHEN I view the UV index
-// THEN I am presented with a color that indicates whether the conditions are favorable, moderate, or severe
-// WHEN I view future weather conditions for that city
-// THEN I am presented with a 5-day forecast that displays the date, an icon representation of weather conditions, the temperature, the wind speed, and the humidity
-// WHEN I click on a city in the search history
-// THEN I am again presented with current and future conditions for that city
+var city = 'austin';
+var stateCode = '78702';
+
+function getApi() {
+  var requestUrl = `http://api.openweathermap.org/data/2.5/forecast?q=${city},${stateCode}&appid=bbb2958c4a7e079d6061f61d0fb13c44`;
+
+  fetch(requestUrl)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+        console.log(data);
+    })
+}
+getApi();
