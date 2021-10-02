185b27cf15577bcb1ca2b25742ae32dde2cb9836
christa-baccas <christa.baccas@gmail.com>
2021-10-02 11:54:46 -0500

    refactored the html page containers

--- script.js
+++ script.js
@@ -1,71 +1,73 @@
-var searchBtn = document.getElementById('search');
-var currentWeatherContainer = document.getElementById('currentWeather');
-var historyContainer = document.getElementById('history');
+var searchBtn = document.getElementById("search");
+var currentWeatherContainer = document.getElementById("currentWeather");
+var historyContainer = document.getElementById("history");
 
 function getApi() {
   // the input value being searched
-  var city = document.getElementById('input').value;
+  var city = document.getElementById("input").value;
   // console.log(city);
   // Api that gives lat and lon when a city is entered
-  var requestUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=bbb2958c4a7e079d6061f61d0fb13c44`; 
+  var requestUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=bbb2958c4a7e079d6061f61d0fb13c44`;
   // fetch calls the API
   fetch(requestUrl)
     //recevie response from API
     .then(function (response) {
-    //response is then converted to json
+      //response is then converted to json
       return response.json();
     })
     //date is then being read
     .then(function (data) {
-        //console.log(data.coord.lon);
-        oneCallApi(data)
-    })
+      //console.log(data.coord.lon);
+      oneCallApi(data);
+    });
 }
 
 // a function to get the lat and lon from the first API call
-function oneCallApi(data){
-  // varaiables that get the lat and lon 
+function oneCallApi(data) {
+  // varaiables that get the lat and lon
   var lat = data.coord.lat;
-  var lon= data.coord.lon;
-  // fetch to the lat and lon API 
+  var lon = data.coord.lon;
+  // fetch to the lat and lon API
   fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=hourly,minutely&units=imperial&appid=bbb2958c4a7e079d6061f61d0fb13c44`)
-  //recevie response from API
-  .then(function (response){
-    //response is then converted to json
-    return response.json()
-    //date is then being read
-  }).then(function (data){
-    console.log(data);
-    renderCurrentWeather(data.current);
-    renderForecast(data.daily);
-  })
+    //recevie response from API
+    .then(function (response) {
+      //response is then converted to json
+      return response.json();
+      //date is then being read
+    })
+    .then(function (data) {
+      console.log(data);
+      renderCurrentWeather(data.current);
+      renderForecast(data.daily);
+    });
 }
 
-function renderCurrentWeather(currentWeather){
+function renderCurrentWeather(currentWeather) {
   console.log(currentWeather);
-  var temp = document.querySelector('.card-temp');
-  var humidity = document.querySelector('.card-humidity');
-  var wind = document.querySelector('.card-wind');
-  var uvi = document.querySelector('.card-uvi');
-  var mainWeather = document.getElementById('mainWeather');
-  mainWeather.setAttribute('style', 'border: 2px solid black');
+  var temp = document.querySelector(".card-temp");
+  var humidity = document.querySelector(".card-humidity");
+  var wind = document.querySelector(".card-wind");
+  var uvi = document.querySelector(".card-uvi");
+  var mainWeather = document.getElementById("mainWeather");
+  mainWeather.setAttribute("style", "border: 2px solid black");
   temp.textContent = "Temp: " + currentWeather.temp + " \u00B0 F";
   humidity.textContent = "Humidity: " + currentWeather.humidity + " %";
   wind.textContent = "Wind: " + currentWeather.wind_speed + " MPH";
   uvi.textContent = "UV Index: " + currentWeather.uvi;
 
   // current date
-  var currentDate = moment().format('L');
-  console.log(currentDate);
+  var currentDate = moment().format("L");
+  // console.log(currentDate);
 
   // sets the header of the current weather to the city being searched
-  var enteredTxt = document.getElementById('input').value;
-  var cardTitle = document.querySelector('.card-title');
-  cardTitle.setAttribute
-  cardTitle.textContent = enteredTxt.charAt(0).toUpperCase() + enteredTxt.slice(1) + " " +currentDate;
+  var enteredTxt = document.getElementById("input").value;
+  var cardTitle = document.querySelector(".card-title");
+  // cardTitle.setAttribute;
+  cardTitle.textContent = enteredTxt.charAt(0).toUpperCase() +enteredTxt.slice(1) + " " + currentDate;
 }
 
-function renderForecast(forecast){
+function renderForecast(forecast) {
+
 }
 
 // when search button is clicked the getAPI function will run
@@ -74,10 +76,9 @@ searchBtn.onclick = getApi;
 //store searches in an array to local storage
 var searchedCities = [];
 
-
-searchBtn.addEventListener('click', function(){
+searchBtn.addEventListener("click", function () {
   // city that is entered in the search box
-  var enteredTxt = document.getElementById('input').value;
+  var enteredTxt = document.getElementById("input").value;
   // push data into the empty array (seachedCities)
   searchedCities.push(enteredTxt);
   // console.log("searched: "+ searchedCities)
@@ -89,11 +90,11 @@ searchBtn.addEventListener('click', function(){
 // console.log(historyContainer);
 
 // this append those searched cities to the HTML page
-function searchHistory(){
-    var searchBtn = document.createElement('button');
-    var enteredTxt = document.getElementById('input').value;
-    searchBtn.setAttribute('class','btn btn-secondary btn-lg col');
-    searchBtn.setAttribute('type','button');
-    searchBtn.textContent = enteredTxt.charAt(0).toUpperCase() + enteredTxt.slice(1);;
-    historyContainer.append(searchBtn);
+function searchHistory() {
+  var searchBtn = document.createElement("button");
+  var enteredTxt = document.getElementById("input").value;
+  searchBtn.setAttribute("class", "btn btn-secondary btn-lg col");
+  searchBtn.setAttribute("type", "button");
+  searchBtn.textContent = enteredTxt.charAt(0).toUpperCase() + enteredTxt.slice(1);
+  historyContainer.append(searchBtn);
 }
